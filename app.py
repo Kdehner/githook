8f505aed39d97a08fbb2d1a42bd8bcc026362b68
Kdehner <Kdehner@gmail.com>
2019-05-21 11:09:11 -0600

    function to start app with (default)provided port

--- app.py
+++ app.py
@@ -89,6 +89,8 @@ def main():
     except Exception:
         return Response(status=401)
 
+def start_app(port='5000'):
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     # So. Many. Checks.
